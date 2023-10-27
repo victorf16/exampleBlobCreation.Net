78955639d8da34bcb845f6ff2c1cfaf7fcb112d4
Vitao <victorf_16@hotmail.com>
2023-10-27 00:38:22 -0300

    first commit

--- Program.cs
+++ Program.cs
@@ -2,13 +2,19 @@
 /* Creating Scripts Container IN azure Storage account */
 
 using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 
-Console.WriteLine("Hello, World!");
-string connectionString = "DefaultEndpointsProtocol=https;AccountName=vitaostoragecool;AccountKey=uJO6I6ZfPP2OstDo5bbx5s8wCfmAjEGQ/0Qf2ZKBV4N169Kw0gu0J8ONhLlgfsxhRgDVZtxHOCQe+ASt20aNGg==;EndpointSuffix=core.windows.net";
-string containerName = "scripts1";
 
-BlobServiceClient blobService
-     = new BlobServiceClient(connectionString);
-await blobService.CreateBlobContainerAsync(containerName);
-Console.WriteLine("Container Created");
+string connectionString = "DefaultEndpointsProtocol=https;AccountName=vitaostoragecool;AccountKey=A8fLCpZMZybiHExeY2MX3/D4nGify1kMpADu1+ijEF9obgOCEVtiuZaj5q2uQmEujfGQDGC8Y0jf+ASt54mwKQ==;EndpointSuffix=core.windows.net";
+string containerName = "scripts";
+string blobName = "scripts.sql"; // insert file name that you want to upload in the destiny
+string filePath = "C:\\Users\\victo\\OneDrive\\Documentos\\PROJETOS .NET\\BlobApp\\scripts.sql.accdb"; // use the path of file
+
+
+BlobContainerClient blobService
+     = new BlobContainerClient(connectionString,containerName);
+
+BlobClient blobCriente=blobService.GetBlobClient(blobName);
+await blobCriente.UploadAsync(filePath, true); // true ensure to update currect file and uploadasync ensure to sent it in assincrounisly mode
+
 
